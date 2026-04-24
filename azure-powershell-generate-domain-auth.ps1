474ec63de6b8560f492006079c8cba079a26f405
avEggHead <usa.pants45@gmail.com>
2026-04-24 15:51:47 -0400

    first commit

---
+++ azure-powershell-generate-domain-auth.ps1
@@ -0,0 +1,3 @@
+Invoke-AzRestMethod `
+  -Path "/subscriptions/<subscription id>/resourceGroups/<resource group>/providers/Microsoft.DomainRegistration/domains/<domain>/transferout?api-version=2021-02-01" `
+  -Method PUT
