a1b9b84b5d7a7499a67a0582be28e677877bcad2
avEggHead <usa.pants45@gmail.com>
2026-04-24 15:53:18 -0400

    comments on azure ps1 command for domain EPP generation

--- azure-powershell-generate-domain-auth.ps1
+++ azure-powershell-generate-domain-auth.ps1
@@ -1,3 +1,6 @@
+#this will generate the EPP code or auth code that you can use to transfer a domain from azure to another registrar
+#allowing the sale of a domain purchased through Azure cloud.
+
 Invoke-AzRestMethod `
-  -Path "/subscriptions/<subscription id>/resourceGroups/<resource group>/providers/Microsoft.DomainRegistration/domains/<domain>/transferout?api-version=2021-02-01" `
-  -Method PUT
+    -Path "/subscriptions/<subscription id>/resourceGroups/<resource group>/providers/Microsoft.DomainRegistration/domains/<domain>/transferout?api-version=2021-02-01" `
+    -Method PUT
